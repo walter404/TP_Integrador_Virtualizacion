766a672e320a692ce17b507707495978f97c640a
walter404 <walterame123@gmail.com>
2025-06-03 16:09:12 -0300

    estructura del codigo

---
+++ main.py
@@ -0,0 +1,12 @@
+notas = []
+
+for i in range(3):
+    nota_valida = False #controlar si la nota es valida
+
+
+
+
+suma = sum(notas)
+promedio = suma / len(notas)
+
+print(f"El promedio de las notas es: {promedio}")
